2e6e77c5da0e8219b64cfa9bb5b9751eebbf1a58
Marsyanda <97005301+marsya612@users.noreply.github.com>
2026-04-24 14:18:11 +0700

    Modify ExampleTest to use actingAs with User
    Updated test to use authenticated user for request.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create([
+            'email_verified_at' => now(), // 🔥 wajib karena pakai 'verified'
+        ]);
+
+        $response = $this->actingAs($user)->get('/');
 
         $response->assertStatus(200);
     }
